2de18fcecaa1e3e1e806dd87241b32f3cc95903f
unknown <wclaybaugh@gmail.com>
2018-10-17 11:30:28 -0400

    fix indentation

--- test_roots.py
+++ test_roots.py
@@ -3,23 +3,23 @@ import roots
 
 def test_quadroots_result():
 	"meaningless change"
-    assert roots.quad_roots(1.0, 1.0, -12.0) == ((3+0j), (-4+0j))
+	assert roots.quad_roots(1.0, 1.0, -12.0) == ((3+0j), (-4+0j))
 
 def test_quadroots_types():
-    with pytest.raises(TypeError):
-        roots.quad_roots("", "green", "hi")
+	with pytest.raises(TypeError):
+		roots.quad_roots("", "green", "hi")
 
 def test_quadroots_zerocoeff():
-    with pytest.raises(ValueError):
-        roots.quad_roots(a=0.0)
+	with pytest.raises(ValueError):
+		roots.quad_roots(a=0.0)
 
 def test_linearoots_result():
-    assert roots.linear_roots(2.0, -3.0) == 1.5
+	assert roots.linear_roots(2.0, -3.0) == 1.5
 
 def test_linearroots_types():
-    with pytest.raises(TypeError):
-        roots.linear_roots("ocean", 6.0)
+	with pytest.raises(TypeError):
+		roots.linear_roots("ocean", 6.0)
 
 def test_linearroots_zerocoeff():
-    with pytest.raises(ValueError):
-        roots.linear_roots(a=0.0)
+	with pytest.raises(ValueError):
+		roots.linear_roots(a=0.0)
